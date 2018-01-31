fdd4d63521f899bc85f248c93c1e02f73bafb6e8
milen-vm <bay_milen@abv.bg>
2018-01-31 22:14:12 +0200

    Sample class with to string method.

---
+++ JavaScriptClasses/Rat.js
@@ -0,0 +1,38 @@
+class Rat {
+
+    constructor (name){
+        this.name = name;
+        this.united = [];
+    }
+
+    unite(otherRat) {
+        if (otherRat instanceof Rat) {
+            this.united.push(otherRat);
+        }
+    }
+
+    getRats() {
+        return this.united;
+    }
+
+    toString() {
+        let result = this.name + '\n';
+
+        for (let i = 0; i < this.united.length; i++) {
+            result += `##${this.united[i].name}\n`;
+        }
+
+        return result.trim();
+    }
+}
+
+let test = new Rat('Pesho');
+console.log(test.toString());
+
+console.log(test.getRats());
+
+test.unite(new Rat('Gosho'));
+test.unite(new Rat('Sasho'));
+console.log(test.getRats());
+
+console.log(test.toString());
